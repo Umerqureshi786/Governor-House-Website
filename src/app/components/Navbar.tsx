24f62e18902a1808810e30c88306e554f70bd977
Umar Qureshi <161929656+Umerqureshi786@users.noreply.github.com>
2025-02-01 17:15:31 +0500

    completed

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,15 +3,17 @@ import React from 'react'
 import Image from 'next/image'
 const Navbar = () => {
     return (
-        <div className='flex justify-around bg-sky-900 h-16'>
+        <div className='flex justify-around bg-sky-900 h-20 gap-2'>
 
-            <div className='mt-5 ml-96'> <Image src={"/Logo.png"} alt='logo' width={80} height={75} /> </div>
+            <div className='mt-5 ml-1 lg:ml-60'>
+                <Image src={"/Logo.png"} alt='logo' width={80} height={75} />
+            </div>
 
-            <div className=' mt-4 text-xl font-bold text-sky-200 ml-24'> <h1>Tuition Free Education Program on Latest Technologies</h1>
+            <div className=' mt-4 text-xl font-bold text-sky-200 ml-16'> <h1>Tuition Free Education Program on Latest Technologies</h1>
             </div>
 
             <div className='ml-auto'>
-                <nav className='flex text-white mt-4 space-x-8 mr-96'>
+                <nav className='flex text-white mt-4 space-x-8 mr-80'>
                     <Link href="/home">Home</Link>
                     <Link href="/apply">Apply</Link>
                     <Link href="/jobs">Jobs</Link>
@@ -20,7 +22,7 @@ const Navbar = () => {
                 </nav>
             </div>
 
-           
+
 
         </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,10 @@ const Home = () => {
         <div>
             <Navbar />
 
-            <div className='flex justify-evenly mt-10 '>
-                <div className='m-12 ml-64 mt-16'>
-                    <h1 className='font-extrabold text-6xl text-sky-800'>Governor Sindh </h1><p className='text-4xl text-sky-800 tracking-widest'>Kamran Khan Tessori</p> <br />
+            <div className='mt-10 sm:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 '>
+                <div className='m-12 ml-6 mt-16 sm:ml-10 md:ml-24 lg:ml-60 '>
+                    <h1 className='font-extrabold text-4xl text-sky-800'>Governor Sindh </h1>
+                    <p className='text-3xl text-sky-800 tracking-widest'>Kamran Khan Tessori</p> <br />
                     <h1 className='text-sky-500 text-4xl font-extrabold tracking-wider'>Certified Cloud <br /> Applied Generative AI <br /> Engineer (GenEng)</h1> <br />
                     <p className='text-2xl text-sky-800 font-extrabold'>Earn up to $5,000 / month</p>
                     <p className='text-2xl text-sky-800 font-extrabold mt-4'>Now admissions are open in <br />Hyderabad</p>
@@ -30,15 +31,15 @@ const Home = () => {
 
 
 
-            <div className='mt-8'>
-                <p className=' ml-96 mr-96 text-xl text-justify'>The pace of technological change is accelerating, big players like Microsoft, Amazon, Google, and OpenAI are winning by providing infrastructure, large AI foundation models, frameworks, 3D Metaverse experiences, and massive distribution networks. Solopreneurs trained in this program will win by automating work typically outsourced to employees, by directly connecting to customers by eliminating the middleman, and by developing vertical metaverses, thus paving the way for the first billion-dollar valued solopreneur businesses. This program has the objective to train this new breed of billion-dollar solopreneurs. These solopreneurs will adopt the ultra-lean business model and work independently and will not need to hire employees or other team members.
+            <div className='mt-8  lg:w-full lg:px-72'>
+                <p className=' px-6 text-xl text-justify '>The pace of technological change is accelerating, big players like Microsoft, Amazon, Google, and OpenAI are winning by providing infrastructure, large AI foundation models, frameworks, 3D Metaverse experiences, and massive distribution networks. Solopreneurs trained in this program will win by automating work typically outsourced to employees, by directly connecting to customers by eliminating the middleman, and by developing vertical metaverses, thus paving the way for the first billion-dollar valued solopreneur businesses. This program has the objective to train this new breed of billion-dollar solopreneurs. These solopreneurs will adopt the ultra-lean business model and work independently and will not need to hire employees or other team members.
 
                 </p>
             </div>
 
 
 
-            <div className='flex justify-center gap-6 mt-8'>
+            <div className='grid grid-cols-1 ml-10 gap-6 mt-8 lg:grid-cols-3  lg:px-64  md:grid-cols-3 md:px-56'>
 
                 <div><Image src={"/imageWebsite.jpg"} alt='Image' width={360} height={360} className='rounded-xl' /></div>
 
@@ -48,28 +49,28 @@ const Home = () => {
 
             </div>
 
-            <div className='border-2 border-black mt-16'>
+            {/* <div className='border-2 border-black mt-16'>
                 <p>Scrol images</p>
-            </div>
+            </div> */}
 
 
 
-            <div className='ml-96'>
+            <div className=''>
 
                 <div>
-                    <h1 className='text-4xl mt-16 font-bold text-sky-800'>Core Courses Sequence</h1>
+                    <h1 className='ml-4 text-4xl mt-16 font-bold text-sky-800 lg:ml-64'>Core Courses Sequence</h1>
 
                 </div >
 
                 <br /> <br />
 
-                <div className='grid grid-cols-4 '>
+                <div className='ml-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:ml-0 lg:px-80'>
 
-                    <div className=' border-black text-center rounded-md items-center flex flex-col justify-center w-80'> <Image src={"/programming_fundamentals.jpg"} alt='programming fundamentals' height={350} width={300} className='rounded-t-xl' />  <div className='bg-white text-lg font-bold rounded-b-lg mt-4 text-neutral-700'>Programming Fundamentals</div>
+                    <div className='  border-black text-center rounded-md items-center flex flex-col justify-center w-80'> <Image src={"/programming_fundamentals.jpg"} alt='programming fundamentals' height={500} width={500} className='rounded-t-xl' />  <div className='bg-white text-lg font-bold rounded-b-lg mt-4 text-neutral-700'>Programming Fundamentals</div>
                     </div>
 
-                    <div className=' border-black text-center rounded-md items-center flex flex-col justify-center w-80'> <Image src={"/nextjs..jpg"} alt='Web2 & nextjs' height={350} width={300} className='rounded-t-xl' /> <div className='bg-white text-lg font-bold rounded-b-lg mt-4 text-neutral-700'> Web2 Using NextJS </div></div>
-                    <div className=' border-black text-center rounded-md items-center flex flex-col justify-center w-80'> <Image src={"/earn_as_your_learn.jpg"} alt='Earn as you learn' height={350} width={300} className='rounded-t-xl' /> <div className='bg-white text-lg font-bold rounded-b-lg mt-4 text-neutral-700 '>Earn as You Learn</div></div>
+                    <div className=' mt-8 border-black text-center rounded-md items-center flex flex-col justify-center w-80'> <Image src={"/nextjs..jpg"} alt='Web2 & nextjs' height={500} width={500} className='rounded-t-xl' /> <div className='bg-white text-lg font-bold rounded-b-lg mt-4 text-neutral-700'> Web2 Using NextJS </div></div>
+                    <div className='mt-8 border-black text-center rounded-md items-center flex flex-col justify-center w-80'> <Image src={"/earn_as_your_learn.jpg"} alt='Earn as you learn' height={500} width={500} className='rounded-t-xl' /> <div className='bg-white text-lg font-bold rounded-b-lg mt-4 text-neutral-700 '>Earn as You Learn</div></div>
 
                 </div>
 
@@ -80,36 +81,36 @@ const Home = () => {
 
 
 
-            <div className='ml-96' >
+            <div className='' >
 
 
-                <div className='text-4xl mt-16 font-bold text-sky-800'>
+                <div className='ml-16 text-4xl mt-16 font-bold text-sky-800 md:ml-64 lg:ml-64 '>
                     Advanced Courses
                 </div>
                 <br /> <br />
 
-                <div className='grid grid-cols-4'>
-                    <div className='  border-black text-center rounded-md items-center flex flex-col justify-center w-80'> <Image src={"/AI.jpg"} alt='AI' height={350} width={300} className='rounded-t-xl' /> <div className='bg-white text-lg font-bold rounded-b-lg mt-4 text-neutral-700'>Artificial Intelligence</div></div>
+                <div className='grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 md:px-56 lg:px-60'>
+                    <div className='  border-black text-center rounded-md items-center flex flex-col justify-center w-full'> <Image src={"/AI.jpg"} alt='AI' height={350} width={300} className='rounded-t-xl' /> <div className='bg-white text-lg font-bold rounded-b-lg mt-4 text-neutral-700'>Artificial Intelligence</div></div>
 
-                    <div className='  border-black text-center rounded-md items-center flex flex-col justify-center w-80'> <Image src={"/metaverse.jpg"} alt='Metaverse' height={350} width={300} className='rounded-t-xl' /> <div className='bg-white text-lg font-bold rounded-b-lg mt-4 text-neutral-700'> Web 3 and Metaverse </div> </div>
-                    <div className='  border-black text-center rounded-md items-center flex flex-col justify-center w-80'> <Image src={"/cloudComputing.jpg"} alt='Cloud Computing' height={350} width={300} className='rounded-t-xl' /> <div className='bg-white text-lg font-bold rounded-b-lg mt-4 text-neutral-700'>Cloud-Native Computing </div> </div>
-                    <div className='  border-black text-center rounded-md items-center flex flex-col justify-center w-80'> <Image src={"/iot.jpg"} alt='Iot' height={350} width={300} className='rounded-t-xl' /> <div className='bg-white text-lg font-bold rounded-b-lg mt-4 text-neutral-700'> Ambient Computing and IOT </div></div>
-                    <div className='  border-black text-center rounded-md items-center flex flex-col justify-center w-80'><Image src={"/genomics.jpg"} alt='Iot' height={350} width={300} className='rounded-t-xl' /> <div className='bg-white text-lg font-bold rounded-b-lg mt-4 text-neutral-700'> Genomics and Bioinformatics </div></div>
-                    <div className=' mt-7 border-black text-center rounded-md items-center flex flex-col justify-center w-80'><Image src={"/automation.jpg"} alt='Iot' height={350} width={300} className='rounded-t-xl' /> <div className='bg-white text-lg font-bold rounded-b-lg mt-4 text-neutral-700'> Network Programmability and Automation </div></div>
+                    <div className='  border-black text-center rounded-md items-center flex flex-col justify-center w-full'> <Image src={"/metaverse.jpg"} alt='Metaverse' height={350} width={300} className='rounded-t-xl' /> <div className='bg-white text-lg font-bold rounded-b-lg mt-4 text-neutral-700'> Web 3 and Metaverse </div> </div>
+                    <div className='  border-black text-center rounded-md items-center flex flex-col justify-center w-full'> <Image src={"/cloudComputing.jpg"} alt='Cloud Computing' height={350} width={300} className='rounded-t-xl' /> <div className='bg-white text-lg font-bold rounded-b-lg mt-4 text-neutral-700'>Cloud-Native Computing </div> </div>
+                    <div className='  border-black text-center rounded-md items-center flex flex-col justify-center w-full'> <Image src={"/iot.jpg"} alt='Iot' height={350} width={300} className='rounded-t-xl' /> <div className='bg-white text-lg font-bold rounded-b-lg mt-4 text-neutral-700'> Ambient Computing and IOT </div></div>
+                    <div className='  border-black text-center rounded-md items-center flex flex-col justify-center w-full'><Image src={"/genomics.jpg"} alt='Iot' height={350} width={300} className='rounded-t-xl' /> <div className='bg-white text-lg font-bold rounded-b-lg mt-4 text-neutral-700'> Genomics and Bioinformatics </div></div>
+                    <div className=' mt-7 border-black text-center rounded-md items-center flex flex-col justify-center w-full'><Image src={"/automation.jpg"} alt='Iot' height={350} width={300} className='rounded-t-xl' /> <div className='bg-white text-lg font-bold rounded-b-lg mt-4 text-neutral-700'> Network Programmability and Automation </div></div>
                 </div>
 
 
             </div>
 
-            <br /> <br /> <br /> <br />
+            <br /> <br /> <br />
 
-            <div className='bg-slate-100 p-10 flex justify-center gap-40 '>
-
-
-                <div className='flex flex-col gap-2  mr-6'> <h2 className='text-xl font-bold'>Core Courses</h2>  <a href="#">Programming Fundamentals</a><a href="#">Web2 Using Nextjs </a><a href="#">Earn as You Learn</a></div>
+            <div className=' p-10 flex flex-col gap-12 md:flex-row md:justify-center lg:flex-row lg:gap-52 lg:justify-center '>
 
 
-                <div className='flex flex-col gap-2  mr-12'> <h2 className='text-xl font-bold'>Advanced Courses</h2>  <a href="#">Artificial Intelligence and Deep Learning</a><a href="#">Web3 and Metaverse</a><a href="#"> Cloud-Native Computing</a><a href="#">Ambient Computing and IOT</a><a href="#">Genomics and Bioinformatics</a><a href="#">Network Programmability and Automation </a></div>
+                <div className='flex flex-col gap-2'> <h2 className='text-xl font-bold'>Core Courses</h2>  <a href="#">Programming Fundamentals</a><a href="#">Web2 Using Nextjs </a><a href="#">Earn as You Learn</a></div>
+
+
+                <div className='flex flex-col gap-2 '> <h2 className='text-xl font-bold'>Advanced Courses</h2>  <a href="#">Artificial Intelligence and Deep Learning</a><a href="#">Web3 and Metaverse</a><a href="#"> Cloud-Native Computing</a><a href="#">Ambient Computing and IOT</a><a href="#">Genomics and Bioinformatics</a><a href="#">Network Programmability and Automation </a></div>
 
 
 
